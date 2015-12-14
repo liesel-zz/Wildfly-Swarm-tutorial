52118ab81913012c27d73ea73373fd9b8f174b7e
Joel Backschat <joel@cafecomjava.com.br>
2015-12-14 00:07:02 -0200

    Colocando o projeto no git.

---
+++ swarm/src/main/java/br/com/swarm/swarm/HelloServlet.java
@@ -0,0 +1,25 @@
+package br.com.swarm.swarm;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class HelloServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello from Servlet at " + new java.util.Date());
+    }
+	
+
+}
